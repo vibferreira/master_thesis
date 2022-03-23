ea0b9f70f8e1ac759acfd6404156d6d2a87dd210
vibferreira <ferreiravitoriabarbosa@gmail.com>
2022-03-23 13:50:45 +0100

    make grid implemented

--- utils/utis.py
+++ utils/utis.py
@@ -1,13 +1,18 @@
 '''Defines utils functions to save and plot  '''
+''' Functions to plot using torchvision make_grid are from https://medium.com/analytics-vidhya/do-you-visualize-dataloaders-for-deep-neural-networks-7840ae58fee7'''
 
 import glob
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+from torchvision.utils import make_grid
+
 
 import torch
 
-def plot_comparison(x, pred, y):
+def plot_comparison(x:torch.Tensor, 
+                    pred:torch.Tensor, 
+                    y:torch.Tensor) -> None:
     gt = np.squeeze(y.data.cpu().numpy()[0])
     pred = np.squeeze(pred.sigmoid().numpy())
     img = np.squeeze(x.data.cpu().numpy()[0])
@@ -37,3 +42,31 @@ def save_best_model(model,
     acc = float(val_dic['val_accuracy'][-1])
     [os.remove(f) for f in glob.glob(dest_path + '/*')] # remove previous saved files 
     return torch.save(model.state_dict(), dest_path + f'/best_model_epoch_{e + 1}_acc_{round(acc,3)}_iou_{round(iou, 3)}.pth')
+
+
+def plot_grids(grids, titles = ["Input", 'Target']):
+    nrow = len(grids)
+    fig = plt.figure(figsize=(8, nrow), dpi=300)
+    # Remove the space between the grids
+    fig.subplots_adjust(wspace=0, hspace=0)
+    # Each grid is new subplot
+    for i in range(1,nrow+1):
+        sub = fig.add_subplot(nrow,1,i)
+        # Dont show the x-axis
+        sub.xaxis.set_visible(False)
+        # Remove y axis ticks and set yaxis label
+        sub.set_yticks([])
+        sub.set_ylabel(titles[i-1], rotation=0, fontsize=5, labelpad=2)
+        sub.imshow(grids[i-1])
+    plt.show()
+
+def create_grids(imgList, nrofItems, pad, norm = True):
+    '''Create a list of Image grids from Torch tensors'''
+    # Permute the axis as numpy expects image of shape (H x W x C) 
+    return list(map(lambda imgs: make_grid(imgs[:nrofItems], normalize=norm, padding = pad, nrow=nrofItems).permute(1, 2, 0), imgList))
+
+# Create list of two image grids - Input Images & Target Mask Images for Segmentation task
+def create_segement_grids(loader_iter, nrofItems = 5, pad = 4):
+    '''# Create list of two image grids - Input Images & Target Mask Images for Segmentation task'''
+    inp, target = next(loader_iter)
+    return create_grids([inp, target], nrofItems, pad)
