9e916d66787090eb2c8886f05eaba5eb74e5e65d
vibferreira <ferreiravitoriabarbosa@gmail.com>
2022-08-04 15:37:10 +0200

    Post .gitignore changes

--- .ipynb_checkpoints/main-checkpoint.py
+++
@@ -1,5 +0,0 @@
-''' Should include all the necessary info to run the model'''
-
-from scripts import get_patches
-
-import bolinha
--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 ''' Should include all the necessary info to run the model'''
 
 from scripts import get_patches
+from scripts import get_GT
+
+# get ground truth
+
+# Define the paths of the images, mask layers and grid
+# MASKS_PATH = 'data/geopackages/UPDATED_6_mask_per_year.gpkg'
+# GRID_PATH = 'data/geopackages/grid.gpkg'
+# DEST_PATH = 'data/masks'
+
+# gt = get_GT.Get_Ground_Truth(GRID_PATH, MASKS_PATH, DEST_PATH)
+# print(gt.get_items())
+
+# # Delete the temp files 
+# [os.remove(f) for f in glob.glob(DEST_PATH + '/*.tif') if f.endswith('_temp.tif') ]
+
+# get patches 
+
